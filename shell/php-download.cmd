96635c2189b0e20cc1a5d2724563227e9c11db60
Andrey Helldar <helldar@dragon-code.pro>
2023-06-09 19:06:57 +0300

    Bump XDebug version

--- shell/php-download.cmd
+++ shell/php-download.cmd
@@ -6,7 +6,7 @@ set version=%version:"=%
 set minorVersion=%version:"=%
 set vc=%vc:"=%
 
-set xdebugVersion=3.2.0
+set xdebugVersion=3.2.1
 set redisVersion=5.3.7
 set yamlVersion=2.2.2
 set sqlsrvVersion=5.10.0
