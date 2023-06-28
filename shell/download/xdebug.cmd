183ac555cb908c71c2359d92740ef0a3c0a9121c
Andrey Helldar <helldar@dragon-code.pro>
2023-06-29 01:23:07 +0300

    Path bug fixed

--- shell/download/xdebug.cmd
+++ shell/download/xdebug.cmd
@@ -4,7 +4,7 @@ set xdebugProfilerDir=%phpPath%\xdebug\profiler
 
 powershell -Command "Invoke-WebRequest https://xdebug.org/files/php_xdebug-%xdebugVersion%-%minorVersion%-%vc%-nts-x86_64.dll -OutFile %xdebugOutputFile%"
 
-powershell -Command "(Get-Content %phpPath%\php.ini) -replace ';?(xdebug\.output_dir)\s?=(.*)', '$1 = \"%xdebugProfilerDir%"' | Out-File -Encoding \"UTF8\" %phpPath%\php.ini"
+powershell -Command "(Get-Content %phpPath%\php.ini) -replace ';?(xdebug\.output_dir)\s?=(.*)', '$1 = \"%xdebugProfilerDir%\"' | Out-File -Encoding \"UTF8\" %phpPath%\php.ini"
 
 if not exist "%xdebugProfilerDir%" ( mkdir "%xdebugProfilerDir%" )
 
