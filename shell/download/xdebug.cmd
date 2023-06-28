65d4e972748be0cf92eccd1e9eabed5eac2e3981
Andrey Helldar <helldar@dragon-code.pro>
2023-06-29 01:04:15 +0300

    Added profiling config for xDebug

--- shell/download/xdebug.cmd
+++ shell/download/xdebug.cmd
@@ -2,4 +2,6 @@ set xdebugOutputFile=%tmp%\xdebug.dll
 
 powershell -Command "Invoke-WebRequest https://xdebug.org/files/php_xdebug-%xdebugVersion%-%minorVersion%-%vc%-nts-x86_64.dll -OutFile %xdebugOutputFile%"
 
+powershell -Command "(Get-Content %phpPath%\php.ini) -replace ';?(xdebug\.output_dir)\s?=(.*)', '$1 = \"%phpPath%\data\xdebug\profiling"' | Out-File -Encoding \"UTF8\" %phpPath%\php.ini"
+
 move /y %tmp%\xdebug.dll %phpPath%\ext\php_xdebug.dll
