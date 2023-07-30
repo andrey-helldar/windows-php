fef147fbdbfbba177a0eee9fc5489406cd63d082
Andrey Helldar <helldar@dragon-code.pro>
2023-07-30 13:53:29 +0300

    Added redis extension

--- shell/download/redis.cmd
+++ shell/download/redis.cmd
@@ -1,7 +1,1 @@
-set redisOutputFile=%tmp%\redis.zip
-
-powershell -Command "Invoke-WebRequest https://windows.php.net/downloads/pecl/releases/redis/%redisVersion%/php_redis-%redisVersion%-%minorVersion%-nts-%vc%-x64.zip -OutFile %redisOutputFile%"
-
-call %mainPath%\7zip\7za e "%redisOutputFile%" -o"%tmp%\redis" -y
-
-move /y %tmp%\redis\php_redis.dll %phpPath%\ext\php_redis.dll
+copy %mainPath%\extensions\redis\%minorVersion%.dll %phpPath%\ext\php_redis.dll
