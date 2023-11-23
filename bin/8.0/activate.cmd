3772923ea456c59b6160ff15ed88c28ca4f657d3
Andrey Helldar <helldar@dragon-code.pro>
2023-11-23 18:29:30 +0300

    Drop PHP 8.0

--- bin/8.0/activate.cmd
+++
@@ -1,7 +0,0 @@
-@echo off
-
-cd ..\..\shell
-
-call variables.cmd
-
-call php-only.cmd "latest" "8.0" "vs16"
--- bin/8.0/update.cmd
+++
@@ -1,7 +0,0 @@
-@echo off
-
-cd ..\..\shell
-
-call variables.cmd
-
-call main.cmd "latest" "8.0" "vs16" "update"
