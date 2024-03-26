547efc84274a8247ec78d484e9a2b6856e5ac3eb
Andrey Helldar <helldar@dragon-code.pro>
2024-03-26 16:24:28 +0300

    Set `openssl.cafile` option

--- shell/cacert.cmd
+++ shell/cacert.cmd
@@ -1,3 +1,4 @@
 powershell -Command "Invoke-WebRequest https://curl.se/ca/cacert.pem -OutFile %phpPath%\cacert.pem"
 
 powershell -Command "(Get-Content %phpPath%\php.ini) -replace ';?(curl\.cainfo)\s?=(.*)', '$1 = \"%phpPath%\cacert.pem\"' | Out-File -Encoding \"UTF8\" %phpPath%\php.ini"
+powershell -Command "(Get-Content %phpPath%\php.ini) -replace ';?(openssl\.cafile)\s?=(.*)', '$1 = \"%phpPath%\cacert.pem\"' | Out-File -Encoding \"UTF8\" %phpPath%\php.ini"
