5047c92bff33788eac1ba2aebbee4153e5565315
Andrey Helldar <helldar@dragon-code.pro>
2025-01-28 13:05:01 +0300

    Fixed xDebug download command

--- shell/download/xdebug.cmd
+++ shell/download/xdebug.cmd
@@ -2,7 +2,7 @@ set xdebugOutputFile=%tmp%\xdebug.dll
 
 set xdebugProfilerDir=%phpPath%\xdebug\profiler
 
-powershell -Command "Invoke-WebRequest https://xdebug.org/files/php_xdebug-%xdebugVersion%-%minorVersion%-%vc%-nts-x86_64.dll -OutFile %xdebugOutputFile%"
+powershell -Command "Invoke-WebRequest https://xdebug.org/files/php_xdebug-%xdebugVersion%-%minorVersion%-nts-%vc%-x86_64.dll -OutFile %xdebugOutputFile%"
 
 powershell -Command "(Get-Content %phpPath%\php.ini) -replace ';?(xdebug\.output_dir)\s?=(.*)', '$1 = \"%xdebugProfilerDir%\"' | Out-File -Encoding \"UTF8\" %phpPath%\php.ini"
 
