e602d883117a0b27b15c9247bf00b0d6a3226990
Andrey Helldar <helldar@dragon-code.pro>
2026-02-16 15:10:46 +0300

    Fix typo in `php.cmd` download URL template to ensure correct PHP version resolution

--- shell/download/php.cmd
+++ shell/download/php.cmd
@@ -1,5 +1,5 @@
 set phpOutputFile=%tmp%\php.zip
 
-powershell -Command "Invoke-WebRequest https://downloads.php.net/~windows/releases/latest/php-%version%-nts-Win32--%vc%-x64-latest.zip -OutFile %phpOutputFile%"
+powershell -Command "Invoke-WebRequest https://downloads.php.net/~windows/releases/latest/php-%version%-nts-Win32-%vc%-x64-latest.zip -OutFile %phpOutputFile%"
 
 call %mainPath%\7zip\7za x "%phpOutputFile%" -o"%phpPath%" -y
