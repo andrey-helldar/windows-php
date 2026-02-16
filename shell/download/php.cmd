903cb09764e78bdcba9d66985df940843e45a6f2
Andrey Helldar <helldar@dragon-code.pro>
2026-02-16 15:10:11 +0300

    Simplify `php.cmd` by standardizing download logic for latest PHP release

--- shell/download/php.cmd
+++ shell/download/php.cmd
@@ -1,9 +1,5 @@
 set phpOutputFile=%tmp%\php.zip
 
-if /I %type%=="latest" (
-    powershell -Command "Invoke-WebRequest https://windows.php.net/downloads/releases/latest/php-%version%-nts-Win32-%vc%-x64-latest.zip -OutFile %phpOutputFile%"
-) else (
-    powershell -Command "Invoke-WebRequest https://windows.php.net/downloads/releases/archives/php-%version%-nts-Win32-%vc%-x64.zip -OutFile %phpOutputFile%"
-)
+powershell -Command "Invoke-WebRequest https://downloads.php.net/~windows/releases/latest/php-%version%-nts-Win32--%vc%-x64-latest.zip -OutFile %phpOutputFile%"
 
 call %mainPath%\7zip\7za x "%phpOutputFile%" -o"%phpPath%" -y
